f09df4229f4b025b5db51d1c5e429868e8542048
Jason Morrison <jason.p.morrison@gmail.com>
2013-02-24 12:12:52 -0800

    Correctly bind data, no redrawing

--- PolarAreaDiagram.js
+++ PolarAreaDiagram.js
@@ -138,46 +138,95 @@ addLineAxes = function () {
   //        });
 };
 
-var draw = function (val) {
-  var groups,
-      lines,
-      linesToUpdate;
-
-  groups = vizBody.selectAll('.series')
-    .data(timeseries[val]);
-  groups.enter().append("svg:g")
-    .attr('class', 'series')
-    .style('fill', "blue")
-    .style('stroke', "blue");
-
-  groups.exit().remove();
-
-  lines = groups.append('svg:path')
-    .attr("class", "line")
-    .attr("id", "userdata")
-    .attr("d", d3.svg.area.radial()
-        .radius(function (d) { return 0; })
-        .angle(function (d, i) { return (i / axis) * 2 * Math.PI; }))
-    .style("stroke-width", 3)
-    .style("fill", "red")
-    .style("opacity", 0.4);
-
-  lines.attr("d", d3.svg.area.radial()
-      .outerRadius(function (d) { return radius(d); })
-      .innerRadius(function(d) { return 0; })
-      .angle(function (d, i) { return (i / axis) * 2 * Math.PI; }));
-};
+// var draw = function (val) {
+//   var groups,
+//       lines,
+//       linesToUpdate;
+// 
+//   groups = vizBody.selectAll('.series')
+//     .data(timeseries[val]);
+//   groups.enter().append("svg:g")
+//     .attr('class', 'series')
+//     .style('fill', "blue")
+//     .style('stroke', "blue");
+// 
+//   groups.exit().remove();
+// 
+//   lines = groups.append('svg:path')
+//     .attr("class", "line")
+//     .attr("id", "userdata")
+//     .attr("d", d3.svg.area.radial()
+//         .radius(function (d) { return 0; })
+//         .angle(function (d, i) { return (i / axis) * 2 * Math.PI; }))
+//     .style("stroke-width", 3)
+//     .style("fill", "red")
+//     .style("opacity", 0.4);
+// 
+//   lines.attr("d", d3.svg.area.radial()
+//       .outerRadius(function (d) { return radius(d); })
+//       .innerRadius(function(d) { return 0; })
+//       .angle(function (d, i) { return (i / axis) * 2 * Math.PI; }));
+// };
 
 
 var drawBars = function(val) {
-  var groups, bar;
-  pie = d3.layout.pie().value(function(d) { return d; }).sort(null);
-  d = [];
-  for(i = 0; i<timeseries[val][0].length; i++) { d.push(1); }
+
+  window.handleX = function(d, i) {
+    var midAngle =
+      ((d.startAngle + d.endAngle) * 0.5)
+      -  (Math.PI / 2.0); // D3 seems to operate on a different quadrant basis?
+
+    var handleRadius = radius(d.data);
+    return handleRadius * Math.cos(midAngle);
+  };
+
+  window.handleY = function(d, i) {
+    var midAngle =
+      ((d.startAngle + d.endAngle) * 0.5)
+      -  (Math.PI / 2.0); // D3 seems to operate on a different quadrant basis?
+    var handleRadius = radius(d.data);
+    return handleRadius * Math.sin(midAngle);
+  };
+
+
+  var drag = d3.behavior.drag()
+    .on("dragstart", function(d, i) {
+      var slice = d3.select(vizBody.selectAll(".series g.slice path")[0][i]);
+      slice.attr("fill-opacity", 0.4);
+      d3.select(this).style("fill-opacity", 0.6);
+    })
+    .on("dragend", function(d, i) {
+      var slice = d3.select(vizBody.selectAll(".series g.slice path")[0][i]);
+      slice.attr("fill-opacity", 0.6);
+      d3.select(this).style("fill-opacity", 1.0);
+    })
+    .on("drag", function(d,i) {
+
+      var x = d3.event.x;
+      var y = d3.event.y;
+
+      var newMagnitude = Math.sqrt(x * x + y * y);
+      var newValue = radius.invert(newMagnitude);
+
+      if (newValue < minVal) { newValue = minVal; }
+      if (newValue > maxVal) { newValue = maxVal; }
+
+      updateData(val, i, newValue);
+
+      // redraw(0);
+    });
+
+  window.pie = undefined;
+  pie = d3.layout.pie().value(function(d) { return 1; }).sort(null);
+
+  ones = [];
+  for(i = 0; i<timeseries[val][0].length; i++) { ones.push(1); }
   // d = [1,2,3,5,8,11,19,30]; // different pie slice widths
 
-  groups = vizBody.selectAll('.series')
-    .data([d]);
+  var data = timeseries[val][0];
+
+  window.groups = vizBody.selectAll('.series')
+    .data([ones]);
   groups.enter().append("svg:g")
     .attr('class', 'series')
     .style('fill', "blue")
@@ -185,111 +234,56 @@ var drawBars = function(val) {
 
   groups.exit().remove();
 
-  // var colors = ["red", "brown", "yellow", "green", "blue", "indigo", "violet", "black"];
-  var colors = ["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd", "#8c564b", "#e377c2", "#7f7f7f", "#bcbd22", "#17becf"];
+  var color = d3.scale.category10();
 
-  bar = d3.svg.arc()
+  window.bar = d3.svg.arc()
     .innerRadius( innerRadiusIncrement * w / 100.0 )
-    .outerRadius( function(d,i) { return radius( timeseries[val][0][i] ); });
+    .outerRadius( function(d,i) { 
+      return radius( d.data );
+    });
 
-  arcs = groups.selectAll(".series g.arc")
-    .data(pie);
+  window.arcs = groups.selectAll(".series g.arc")
+    .data(pie(data));
 
   arc = arcs
     .enter()
     .append("g")
-    .attr("class", "attr");
-
-
-  arc.append("path")
-    .attr("fill", function(d,i) { return colors[i % 8]; })
-    .attr("d", bar)
-    .style("opacity", 0.6);
-
-  arcs.exit().remove();
+      .attr("class", "slice")
+    .append("path")
+      .attr("fill", function(d,i) { return color(i); })
+      .attr("d", window.bar)
+      .style("opacity", 0.6);
 
   var width = 10;
   var height = 10;
   var handleRad = 10;
 
-  handles = groups.selectAll(".series g.handle")
-    .data(pie)
+  var handles = groups.selectAll(".series g.handle")
+    .data(pie(data))
     .enter()
     .append("g")
-    .attr("class", "attr");
-
-  var handleX = function(d, i) {
-    var midAngle =
-      ((d.startAngle + d.endAngle) * 0.5)
-      -  (Math.PI / 2.0); // D3 seems to operate on a different quadrant basis?
-
-    var handleRadius = radius(timeseries[val][0][i]);
-    return handleRadius * Math.cos(midAngle);
-  };
-
-  var handleY = function(d, i) {
-    var midAngle =
-      ((d.startAngle + d.endAngle) * 0.5)
-      -  (Math.PI / 2.0); // D3 seems to operate on a different quadrant basis?
-    var handleRadius = radius(timeseries[val][0][i]);
-    return handleRadius * Math.sin(midAngle);
-  };
-
-
-  var drag = d3.behavior.drag()
-    .origin(function(d, i) {
-      return {
-        x: handleX(d, i),
-        y: handleY(d, i)
-      };
-    })
-    .on("drag", function(d,i) {
-      var oldX = parseInt(d3.select(this).attr("cx"), 10);
-      var oldY = parseInt(d3.select(this).attr("cy"), 10);
-
-      console.log(d3.select(this).attr("cx"),
-                  d3.select(this).attr("cx"));
-
-      var newX = oldX + d3.event.dx;
-      var newY = oldY + d3.event.dy;
-
-      d3.select(this).attr("cx", newX);
-      d3.select(this).attr("cy", newY);
-      d.x = newX;
-      d.y = newY;
-
-      var svgLeft = $('svg').position().left - $(document).scrollLeft();
-      var svgTop = $('svg').position().top - $(document).scrollTop();
-      var distanceFromCenterX = (centerXPos + svgLeft) - newX;
-      var distanceFromCenterY = (centerYPos  + svgTop) - newY;
-
-      var newMagnitude = Math.sqrt(distanceFromCenterX * distanceFromCenterX + distanceFromCenterY * distanceFromCenterY);
-      var newValue = radius.invert(newMagnitude);
-
-      var factor = newValue / timeseries[val][0][i];
-
-      if (newValue < minVal) { newValue = minVal; }
-      if (newValue > maxVal) { newValue = maxVal; }
-      timeseries[val][0][i] = newValue;
-
-      console.log(timeseries[val][0]);
-
-      redraw(0);
-    });
+    .attr("class", "handle");
 
   handles.append("circle")
     .call(drag)
-    .data(pie)
     .attr("fill", "#ddd")
     .attr("stroke", "#666")
     .attr("stroke-width", "3")
     .attr("cx", handleX)
     .attr("cy", handleY)
     .attr("r", handleRad);
-
 }
 
-function redraw( val ) {        
-  vizBody.selectAll('.series').remove();
-  drawBars( val );
+function updateData(val, i, newValue) {
+  var data = timeseries[val][0];
+  data[i] = newValue;
+
+  vizBody.selectAll('.series g.handle circle')
+    .data(pie(data))
+    .attr("cx", handleX)
+    .attr("cy", handleY);
+
+  vizBody.selectAll(".series g.slice path")
+    .data(pie(data))
+    .attr("d", window.bar);
 }
